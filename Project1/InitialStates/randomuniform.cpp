ebb17ccc4a846f077ee8fa228969a4d0067a4c45
elisabethchr <elisabeth-chr96@hotmail.no>
2020-02-03 17:11:08 +0100

    Added random uniform initial state

--- Project1/InitialStates/randomuniform.cpp
+++ Project1/InitialStates/randomuniform.cpp
@@ -1,17 +1,17 @@
 #include "randomuniform.h"
 #include <iostream>
 #include <cassert>
+#include <armadillo>
 #include "Math/random.h"
 #include "../particle.h"
 #include "../system.h"
 
 using std::cout;
 using std::endl;
+using namespace std;
+using namespace arma;
 
-RandomUniform::RandomUniform(System*    system,
-                             int        numberOfDimensions,
-                             int        numberOfParticles)  :
-        InitialState(system) {
+RandomUniform::RandomUniform(System* system, int numberOfDimensions, int numberOfParticles)  : InitialState(system) {
     assert(numberOfDimensions > 0 && numberOfParticles > 0);
     m_numberOfDimensions = numberOfDimensions;
     m_numberOfParticles  = numberOfParticles;
@@ -27,10 +27,27 @@ RandomUniform::RandomUniform(System*    system,
 }
 
 void RandomUniform::setupInitialState() {
-    for (int i=0; i < m_numberOfParticles; i++) {
-        std::vector<double> position = std::vector<double>();
 
-        for (int j=0; j < m_numberOfDimensions; j++) {
+    // Random number generator
+    random_device rd;
+    mt19937_64 gen(rd());
+    uniform_real_distribution<double> RandomNumberGenerator(0.0,1.0);
+
+    double delta = 0.1;
+    mat position(m_numberOfParticles, m_numberOfDimensions); position.zeros();
+
+
+    // Initialize positions
+    for(int i=0; i<m_numberOfParticles; i++){
+        for(int j=0; j<m_numberOfDimensions; j++){
+            position[i, j] = delta*(RandomNumberGenerator(gen) - 0.5);
+        }
+    }
+
+    for (int i=0; i < m_numberOfParticles; i++) {
+//        std::vector<double> position = std::vector<double>();
+
+//        for (int j=0; j < m_numberOfDimensions; j++) {
             /* This is where you should actually place the particles in
              * some positions, according to some rule. Since this class is
              * called random uniform, they should be placed randomly according
@@ -42,10 +59,12 @@ void RandomUniform::setupInitialState() {
              * according to their index in the particles list (this is
              * obviously NOT a good idea).
              */
-            position.push_back(i);
-        }
+//            position.push_back(i);
+//        }
+
         m_particles.push_back(new Particle());
         m_particles.at(i)->setNumberOfDimensions(m_numberOfDimensions);
         m_particles.at(i)->setPosition(position);
     }
+
 }
