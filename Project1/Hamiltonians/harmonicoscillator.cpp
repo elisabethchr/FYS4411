b65b171a80bc85c89aebaf5ee621c7ee6863336b
fosheimdet <42669087+fosheimdet@users.noreply.github.com>
2020-02-13 19:08:10 +0100

    Add files via upload

--- Project1/Hamiltonians/harmonicoscillator.cpp
+++ Project1/Hamiltonians/harmonicoscillator.cpp
@@ -15,6 +15,7 @@ HarmonicOscillator::HarmonicOscillator(System* system, double omega) :
 }
 
 double HarmonicOscillator::computeLocalEnergy(std::vector<Particle*> particles) {
+
     /* Here, you need to compute the kinetic and potential energies. Note that
      * when using numerical differentiation, the computation of the kinetic
      * energy becomes the same for all Hamiltonians, and thus the code for
@@ -25,6 +26,28 @@ double HarmonicOscillator::computeLocalEnergy(std::vector<Particle*> particles) 
      * m_system->getWaveFunction()...
      */
 
+    double h = m_system->m_stepLength;
+
+    double psi_current = m_system->getWaveFunction()->evaluate(particles, 0.4);//how to get alpha?
+    std::vector<particle*> particles_new = particles;
+
+    for (int i = 0; i<particles_new.size();i++){
+         particles_new->adjust_position(h,0);
+    }
+
+    double psi_new = m_system->getWaveFunction()->evaluate(particles_new,0.4);
+
+
+    particles_new->
+
+
+    int dim = m_system->getNumberOfDimensions();
+    int nPart= m_system->getNumberOfParticles();
+
+
+
+
+    double El = dim*0.5*m_omega*nPart;
     double potentialEnergy = 0;
     double kineticEnergy   = 0;
     return kineticEnergy + potentialEnergy;
