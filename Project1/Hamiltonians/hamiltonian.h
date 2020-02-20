b35b27281ce23283443dace40d46195b1a316bee
elisabethchr <elisabeth-chr96@hotmail.no>
2020-02-20 19:09:37 +0100

    Added analytic energy

--- Project1/Hamiltonians/hamiltonian.h
+++ Project1/Hamiltonians/hamiltonian.h
@@ -5,6 +5,7 @@ class Hamiltonian {
 public:
     Hamiltonian(class System* system);
     virtual double computeLocalEnergy(std::vector<class Particle*> particles) = 0;
+    virtual double computeLocalEnergy_analytic(std::vector<class Particle*> particles) = 0;
 
 protected:
     class System* m_system = nullptr;
