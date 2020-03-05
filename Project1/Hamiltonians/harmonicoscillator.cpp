3a886abd037f7b2acb797d3170dee652a124bb75
Elisabeth Christensen <42673715+elisabethchr@users.noreply.github.com>
2020-03-05 16:07:11 +0100

    Added analytic energy

--- Project1/Hamiltonians/harmonicoscillator.cpp
+++ Project1/Hamiltonians/harmonicoscillator.cpp
@@ -10,7 +10,7 @@ using std::cout;
 using std::endl;
 
 HarmonicOscillator::HarmonicOscillator(System* system, double omega) :
-        Hamiltonian(system) {
+    Hamiltonian(system) {
     assert(omega > 0);
     m_omega  = omega;
 }
@@ -31,29 +31,48 @@ double HarmonicOscillator::computeLocalEnergy(std::vector<Particle*> particles) 
     double kineticEnergy   = 0;
     int dim = m_system->getNumberOfDimensions();
     int nPart= m_system->getNumberOfParticles();
-    std::vector<double> alpha= m_system->getWaveFunction()->getParameters();
+    std::vector<double> alpha = m_system->getWaveFunction()->getParameters();
 
 
 
     for (int i = 0; i<nPart;i++){
-
         for(int j = 0; j<dim; j++){
             double coordinate = particles.at(i)->getPosition().at(j);
             potentialEnergy+= 0.5*m_omega*m_omega*coordinate*coordinate;
         }
-
     }
 
-    double psi = m_system->getWaveFunction()->evaluate(particles,alpha[0]);
+    double psi = m_system->getWaveFunction()->evaluate(particles);
 
     kineticEnergy = -(1/psi)*0.5*m_system->getWaveFunction()->computeDoubleDerivative(particles);
 
-
-    double El = potentialEnergy +kineticEnergy;
-//    cout <<"Local energy:  " << El << endl;
+    double El = potentialEnergy + kineticEnergy;
+    //    cout <<"Local energy:  " << El << endl;
     return El;
-
-
-
 }
+
+
+double HarmonicOscillator::computeLocalEnergy_analytic(std::vector<Particle*> particles) {
+
+    double potentialEnergy = 0;
+    double kineticEnergy   = 0;
+    double coordinate;
+    int dim = m_system->getNumberOfDimensions();
+    int nPart= m_system->getNumberOfParticles();
+    //    std::vector<double> alpha = m_system->getWaveFunction()->getParameters();
+    double alpha = m_system->getWaveFunction()->getParameters()[0];
+    double psi = m_system->getWaveFunction()->evaluate(particles);
+
+    // so far only one dimension, but must calculate coordinates more properly for r_i^2 = x_i^2 + y_i^2 + z_i^2
+    for (int i = 0; i<nPart; i++){
+        for(int j=0; j<dim; j++){
+            coordinate = particles.at(i)->getPosition().at(j);
+            potentialEnergy+= 0.5*m_omega*m_omega*coordinate*coordinate * psi;
+            kineticEnergy -= 0.5*2*alpha*(2*alpha*coordinate*coordinate - dim) * psi;
+        }
+    }
+    double localEnergy_analytic = (kineticEnergy + potentialEnergy) / psi;
+    return localEnergy_analytic;
+}
+
 
