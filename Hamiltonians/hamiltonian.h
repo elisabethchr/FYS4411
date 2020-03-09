a97ad17516775080a660865a7b152355026a2e77
Elisabeth Christensen <42673715+elisabethchr@users.noreply.github.com>
2020-03-09 14:55:54 +0100

    Added quantum force

--- Hamiltonians/hamiltonian.h
+++ Hamiltonians/hamiltonian.h
@@ -1,15 +1,15 @@
 #pragma once
 #include <vector>
+#include <armadillo>
+
+using namespace arma;
 
 class Hamiltonian {
 public:
     Hamiltonian(class System* system);
     virtual double computeLocalEnergy(std::vector<class Particle*> particles) = 0;
     virtual double computeLocalEnergy_analytic(std::vector<class Particle*> particles) = 0;
-    //Remove later: This is a pure virtual function, meaning any derived classes e.g. HarmonicOscillator MUST overwrite it.
-    //In a normal virtual function one would simply omit the "=0", and any derived classes would use the
-    //implementation of the base class if it has not been overwritten. Pure virtual functions are good for
-    //making sure every derived class has its own implementation of the function.
+    virtual mat computeQuantumForce(std::vector<class Particle*> particles) = 0;
 
 protected:
     class System* m_system = nullptr;
