7203b8121ab07ae2a71ae183ee5c28084db1d7b5
elisabethchr <elisabeth-chr96@hotmail.no>
2020-03-21 00:44:35 +0100

    Correct expressions

--- Project1/WaveFunctions/simplegaussian.h
+++ Project1/WaveFunctions/simplegaussian.h
@@ -1,9 +1,25 @@
 #pragma once
+#include <armadillo>
 #include "wavefunction.h"
 
 class SimpleGaussian : public WaveFunction {
 public:
-    SimpleGaussian(class System* system, double alpha);
+    SimpleGaussian(class System* system, std::vector<double> alpha);
     double evaluate(std::vector<class Particle*> particles);
-    double computeDoubleDerivative(std::vector<class Particle*> particles);
+    arma::vec computeGradient(std::vector<class Particle*> particles, int i);
+    double computeDoubleDerivative_numeric(std::vector<class Particle*> particles);
+    double computeDoubleDerivative_analytic(std::vector<class Particle *> particles);
+    double getDistance(std::vector<class Particle *> particles, int particle_i, int particle_j){}
+    double computeLaplacian(std::vector<class Particle*> particles){}
+
+protected:
+    double SingleParticleFunction(std::vector<class Particle *> particles, int particle){}
+    double correlationWaveFunction(std::vector<class Particle *> particles, double distance){}
+    double computeLaplacian_SPF(int particle_i){}
+    arma::vec computeGradient_SPF(int particle_i){}
+    double computeLaplacian_correlation(std::vector<class Particle *> particles, int particle_i){}
+    arma::vec computeGradient_correlation(std::vector<class Particle *> particles, int particle_i){}
+    double computeDerivative_correlation(int particle_i, int particle_j){}
+    double computeDoubleDerivative_correlation(int particle_i, int particle_j){}
+
 };
--- Project1/WaveFunctions/wavefunction.h
+++ Project1/WaveFunctions/wavefunction.h
@@ -1,18 +1,41 @@
 #pragma once
 #include <vector>
-
+#include <armadillo>
 
 class WaveFunction {
 public:
     WaveFunction(class System* system);
-    int     getNumberOfParameters() { return m_numberOfParameters; }
-    std::vector<double> getParameters() { return m_parameters; }
+    int     getNumberOfParameters()     { return m_numberOfParameters; }
+    int     getNumberOfParametersBeta() { return m_numberOfParametersBeta; }
+    double  getHardCoreDiameter()       { return m_hard_core_diameter; }
+    double  getGamma()                  { return m_gamma; }
+    std::vector<double> getParameters()     { return m_parameters; }
+    std::vector<double> getParametersBeta() { return m_beta; }
+
     virtual double evaluate(std::vector<class Particle*> particles) = 0;        //"virtual" implements function elsewhere, "virtual = 0" forces to implemented elsewhere
-    virtual double computeDoubleDerivative(std::vector<class Particle*> particles) = 0;
+    virtual arma::vec computeGradient(std::vector<class Particle*> particles, int i) = 0;
+    virtual double computeLaplacian(std::vector<class Particle*> particles) = 0;
+    virtual double computeDoubleDerivative_numeric(std::vector<class Particle*> particles) = 0;
+    virtual double computeDoubleDerivative_analytic(std::vector<class Particle*> particles) = 0;
+    virtual double getDistance(std::vector<class Particle *> particle, int particle_i, int particle_j) = 0;
 
 protected:
     int     m_numberOfParameters = 0;
+    int     m_numberOfParametersBeta = 0;
+    double  m_hard_core_diameter = 0;
+    double  m_gamma = 2.83843;
+//    double  m_gamma = 1.0;
     std::vector<double> m_parameters = std::vector<double>();
+    std::vector<double> m_beta = std::vector<double>();
     class System* m_system = nullptr;
-};
 
+    virtual double SingleParticleFunction(std::vector<class Particle *> particles, int particle) = 0;
+    virtual double correlationWaveFunction(std::vector<class Particle* > particles, double distance) = 0;
+    virtual double computeLaplacian_SPF(int particle_i) = 0;
+    virtual arma::vec computeGradient_SPF(int particle_i) = 0;
+    virtual double computeLaplacian_correlation(std::vector<class Particle *> particles, int particle_i) = 0;
+    virtual arma::vec computeGradient_correlation(std::vector<class Particle *> particles, int particle_i) = 0;
+    virtual double computeDerivative_correlation(int particle_i, int particle_j) = 0;
+    virtual double computeDoubleDerivative_correlation(int particle_i, int particle_j) = 0;
+
+};
